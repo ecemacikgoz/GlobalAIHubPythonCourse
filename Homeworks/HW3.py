dbe7382765fc9388f11bf0f9ba19bcd901928cb2
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-02-07 10:22:42 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,15 @@
-#Explain your work
+# The prime number has to be greater than 1. There are 25 number between 0-100.
 
-#Question 1
-for x in range(a):
-	print(a)
+def primeNums():
+    for i in range(0, 100+1):
+        if i > 1:
+                for k in range(2, i):
+                    if i % k == 0:
+                        break
+                else:
+                    print(i)          
+        else:
+            continue
+
+            
+primeNums()
